1a7821ab93356e0008a89db3b61eb46e4b64c9d4
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-25 15:27:30 +0200

    managed href in navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,15 @@
 <header>
   <nav class="navbar navbar-expand-lg bg-body-primary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Bikes 2050
-      </a>
+      <span class="navbar-brand">Bikes 2050
+      </span>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Features</a>
