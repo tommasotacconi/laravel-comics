0dc628405502e39f697dc4fbd0a709b9c1d72a77
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-25 15:26:20 +0200

    added name to route to pages.home

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ use Illuminate\Support\Facades\Route;
 // Dati dal db
 
 Route::get('/', function () {
-    $bikes = config('db.bikes');
-    return view('pages.home', compact('bikes'));
-});
+  $bikes = config('db.bikes');
+  return view('pages.home', compact('bikes'));
+})->name('home');
 
 // Rotte per le singole show
 Route::get('/{id}', function (string $bike_id) {
-    $bike = config('db.bikes.' . $bike_id);
-    return view('pages.show', compact('bike'));
+  $bike = config('db.bikes.' . $bike_id);
+  return view('pages.show', compact('bike', 'bike_id'));
 });
 
 // final commit
