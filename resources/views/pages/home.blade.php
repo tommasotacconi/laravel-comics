b9a25f152c05130072c4a20927b915df8942ef6f
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-25 15:29:35 +0200

    reviewed html in home.blade

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,25 +3,21 @@
 @section('page-title', 'Homepage')
 
 @section('main-content')
-
-  {{-- Bikes cards --}}
-  <div class="container-md px-4">
-      <div class="row mt-1 g-3">
-        @foreach ($bikes as $index => $bike)
-          <div class="col-4">
-            <div id="{{ $index }}" class="card" style="">
-              <img src="{{ $bike['img_url'] }}" class="card-img-top" alt="{{ $bike['model_name'] }} bike">
-              <div class="card-body">
-                <h5 class="card-title">{{ $bike['model_name'] }}</h5>
-                <p class="card-text">
-                  {{ $bike['description'] }}
-                </p>
-                <a href="{{ $index }}" class="btn btn-primary">Go to {{ $bike['model_name'] }} </a>
-              </div>
-            </div>
-          </div>
-        @endforeach
-      </div>
-  </div>
+	{{-- Bikes cards --}}
+	<div class="container-md">
+			<div class="row mt-1 mb-4 g-3 ">
+				@foreach ($bikes as $index => $bike)
+					<div class="col-4 col-lg-3">
+						<div id="{{ $index }}" class="card" style="">
+							<img src="{{ $bike['img_url'] }}" class="card-img-top" alt="{{ $bike['model_name'] }} bike">
+							<div class="card-body">
+								<h5 class="card-title">{{ $bike['model_name'] }}</h5>
+								<a href="{{ $index }}" class="btn btn-primary mt-4">Go to {{ $bike['model_name'] }} </a>
+							</div>
+						</div>
+					</div>
+				@endforeach
+			</div>
+	</div>
 
 @endsection
